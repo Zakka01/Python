527d1ccac50b640cbc38a6c0d4a4c068f632008f
Zakariae Ahrabare <ahrabare8@gmail.com>
2026-02-06 22:34:20 +0100

    done

--- Python00/ex7/ft_seed_inventory.py
+++ Python00/ex7/ft_seed_inventory.py
@@ -1,4 +1,4 @@
-def ft_seed_inventory(seed_type: str, quantity: int, unit: str):
+def ft_seed_inventory(seed_type, quantity, unit):
     if unit == "packets":
         print(f"{seed_type} seeds: {quantity} packets available")
     elif unit == "grams":
