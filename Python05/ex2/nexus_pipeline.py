e92fbc847ee6b783ed042ebc787da8065b5b03ef
Your Name <ahrabare8@gmail.com>
2026-03-23 01:03:23 +0100

    python05 - Done

--- Python05/ex2/nexus_pipeline.py
+++ Python05/ex2/nexus_pipeline.py
@@ -5,6 +5,8 @@ from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional, Protocol
 
 
+
+
 class ProcessingStage(Protocol):
 
     def process(self, data: Any) -> Any:
